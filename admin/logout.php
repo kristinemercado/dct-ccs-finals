a59a8ac10d44821753f1f6c12959bb889ccb96a5
kristinemercado <mercadokristine39@gmail.com>
2024-11-21 22:21:07 +0800

    database successfully connected

--- admin/logout.php
+++ admin/logout.php
@@ -1,3 +1,7 @@
 <?php
     // Logout Code Here
+    require_once '../functions.php';// Include functions file
+
+    // Call the logout_user function to handle logout
+    logout_user();
 ?>
--- functions.php
+++ functions.php
@@ -1,3 +1,16 @@
-<?php    
-    // All project functions should be placed here
+<?php
+    function databaseConn(){
+        $host = 'localhost';
+        $user = 'root';
+        $password = ''; // Default for Laragon
+        $database = 'dct-ccs-finals';
+    
+        $connection = new mysqli($host, $user, $password, $database);
+    
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+    
+        return $connection;
+    }
 ?>
