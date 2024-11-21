0d2ace8db2c585ddbf901b76595e7f326fa402ec
kristinemercado <mercadokristine39@gmail.com>
2024-11-21 22:37:21 +0800

    dashboard includes successfully added

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,4 +1,9 @@
+<?php
+    include 'partials/header.php';
+    
+    include 'partials/side-bar.php';
 
+?>
 
 <!-- Template Files here -->
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">    
